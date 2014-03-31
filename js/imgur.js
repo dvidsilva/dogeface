fa847bb720395181e098f07d24e7910c32da84c4
dvid <contacto@dvidsilva.com>
2014-03-31 00:57:17 -0700

    things

---
+++ js/imgur.js
@@ -0,0 +1,24 @@
+var sendToImgur;
+sendToImgur = function() {
+  var authorization;
+  authorization = "Client-ID " + '61569d8b93f7e2b';
+  $.ajax({
+    url: "https://api.imgur.com/3/image",
+    method: "POST",
+    headers: {
+      Authorization: authorization,
+      Accept: "application/json"
+    },
+    data: {
+      image: localStorage.dataBase64.replace(/.*,/,''),
+      type: "base64"
+    },
+    success: function(result) {
+      var id;
+      id = result.data.id;
+      imgurtext.style.display = 'block';
+      imgurtext.href = "https://imgur.com/" + id;
+      imgurtext.innerHTML = "https://imgur.com/" + id;
+    }
+  });
+};
